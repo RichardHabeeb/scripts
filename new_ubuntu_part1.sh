f74089cd53c2069f4cba48be91898ab13d778381
Richard Habeeb <richard.habeeb@gmail.com>
2022-05-16 19:27:13 -0400

    fixes

---
+++ new_ubuntu_part1.sh
@@ -0,0 +1,4 @@
+sudo apt install curl vim zsh git git-lfs fonts-powerline openssh-server
+
+#ZSH SETUP
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- new_ubuntu_part2.sh
+++ new_ubuntu_part2.sh
@@ -1,8 +1,16 @@
-sudo apt install curl vim zsh git git-lfs fonts-powerline openssh-server
+#ZSH SETUP (CONT)
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+chsh -s $(which zsh)
 
-#ZSH SETUP
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed -i 's/ZSH_THEME=".+"/ZSH_THEME="agnoster"/g' ~/.zshrc
+#SSH
+ssh-keygen -t ed25519
+echo "-------SSH PUBLIC KEY-------"
+cat ~/.ssh/id_ed25519.pub
+echo "-------SSH PUBLIC KEY-------"
+
+#GIT SETUP
+git config --global user.name "Richard Habeeb"
+git config --global user.email "richard.habeeb@gmail.com"
 
 #VIM SETUP
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
@@ -10,11 +18,12 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-git clone git://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+git clone https://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 git clone https://github.com/kyuhi/vim-emoji-complete.git ~/.vim/bundle/vim-emoji-complete
 git clone https://github.com/chrisbra/unicode.vim ~/.vim/bundle/unicode.vim
 git clone https://github.com/rhysd/vim-clang-format ~/.vim/bundle/vim-clang-format
 
-
 ln -s $(pwd)/.vimrc ~/.vimrc
+
+
