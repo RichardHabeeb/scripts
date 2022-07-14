32dedbe136a6ad0b6e8ce32fa2874f9fb2514e55
Richard Habeeb <richard.habeeb@gmail.com>
2022-07-14 23:15:11 +0000

    added some deps to noetic

--- rpi.ubuntu.noetic.zsh
+++ rpi.ubuntu.noetic.zsh
@@ -1,10 +1,16 @@
 #!/usr/bin/env zsh
+
+##
+# instructions for Ubuntu 20.04 for RPI
+# ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz
+##
+
 sudo apt install cmake curl
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-noetic-desktop-full
+sudo apt install ros-noetic-desktop-full ros-noetic-rplidar-ros ros-noetic-rosserial-arduino
 echo "source /opt/ros/noetic/setup.zsh" >> ~/.zshrc
 . ~/.zshrc
 
