18be1d22d6930f02600a4f9b33a7c0701b39dd44
Richard Habeeb <richard.habeeb@gmail.com>
2023-11-17 11:26:55 -0500

    apt

--- new_ubuntu_part1.sh
+++ new_ubuntu_part1.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install curl vim zsh git git-lfs fonts-powerline openssh-server
+sudo apt install -y curl vim zsh git git-lfs fonts-powerline openssh-server language-pack-en
+sudo update-locale
 
 #ZSH SETUP
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
